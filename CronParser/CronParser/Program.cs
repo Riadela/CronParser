cb4a3a70b64df05d1850bd8988151e5b2d69a4b6
Riad Eddin El abiad <riadelab.95@gmail.com>
2021-10-27 20:00:58 +0100

    Project set up

---
+++ CronParser/CronParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CronParser
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
