1a1dd7ed1d548cb0ab97c1b816fcf9e981734e90
Riad Eddin El abiad <riadelab.95@gmail.com>
2021-10-27 20:01:05 +0100

    Add pocos

---
+++ CronParser/CronParser/Pocos/CronOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CronParser.Pocos
+{
+    public class CronOutput
+    {
+        public string timeField { get; } = "";
+        public string timeValue { get; } = "";
+
+        public CronOutput(string pField, string pValue)
+        {
+            timeField = pField;
+            timeValue = pValue;
+        }
+    }
+}
---
+++ CronParser/CronParser/Pocos/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CronParser.Pocos
+{
+    public class Unit
+    {
+        public int minValue { get; }
+        public int maxValue { get; }
+        public HashSet<char> acceptedSymbols { get; }
+        public string cronValue { get; }
+
+        public Unit(int minValue, int maxValue, string cronValue, HashSet<char> acceptedSymbols)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.acceptedSymbols = acceptedSymbols;
+            this.cronValue = cronValue;
+        }
+    }
+}
